ad67b389db7ed9ecddb1e879ae669140bd3f61ae
Milad Latif <mlatif01@dcs.bbk.ac.uk>
2019-07-05 11:49:00 +0100

    first commit

---
+++ desktop/src/com/latif/gokart/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.latif.gokart.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.latif.gokart.GoKart;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GoKart(), config);
+	}
+}
